25bf54c6b74fb9d30f4ed79f4672a72ed773f6ab
Erik de Jonge <erik@a8.nl>
2015-10-04 10:49:16 +0200

    Sunday 04 October 2015 (week:39 day:277), 10:49:11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ license: GNU-GPL2
 """
 from setuptools import setup
 setup(name='pytopcmd',
-      version='8',
+      version='9',
       description='Simpler Unix top command implemented in python',
       url='https://github.com/erikdejonge/pytop',
       author='Erik de Jonge',
